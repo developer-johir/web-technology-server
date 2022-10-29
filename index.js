d9637a17fd53788d3ea6be2f7c859bb32d9fa50c
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-10-29 11:02:44 +0600

    deploy test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const categories = require('./data/categories.json');
 
@@ -15,3 +18,5 @@ app.get('/web-categories', (req, res) => {
 app.listen(port, () => {
   console.log('Example app listening on port', port)
 })
+
+module.exports = app;
