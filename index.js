c1cff3a91b4672ba87b619c6aef3dc7e0b068beb
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-10-30 17:46:45 +0600

    Category data added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/web-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(course);
+    }
+    else{
+        const categoryCourse = course.filter(c => c.category_id === id);
+        res.send(categoryCourse);
+    }
+})
+
+app.get('/course', (req, res) => {
+    res.send(course);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find( c => c._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
